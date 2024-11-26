0920696310a6fc80c68289aab7bcd66b86337df0
kshitij <rajkshitij99@gmail.com>
2024-11-26 22:07:21 +0530

    mongodb implemented

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,23 @@
 import express from "express";
 import cors from "cors";
 import data from "./data.js";
+import mongoose from "mongoose";
+import config from "./config.js";
+import userRouter from "./routers/userRouter.js";
+mongoose.connect(config.MONGODB_URL,{
+  useNewUrlParser:true,
+  useUnifiedTopology:true,
+}).then(()=>{
+  console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>connected to mongodb");
+  
+}).catch((error)=>{
+  console.log(error.message);
+  
+})
 const app = express();
 
 app.use(cors());
+app.use('/api/users',userRouter)
 
 app.get("/api/products", (req, res) => {
   res.send(data.products);
